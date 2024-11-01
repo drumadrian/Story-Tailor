4fb9cb1dc245258dbde9f09eedf61dfc97b43c6c
Adrian Drummond <adrian.drummond@gmail.com>
2024-10-31 20:58:35 -0700

    updating config

--- Story-Tailor/app.config.js
+++ Story-Tailor/app.config.js
@@ -1,23 +1,58 @@
 import 'expo-env';
 
 export default {
-  expo: {
-    name: "Story Tailor",
-    slug: "Story-Tailor",
-    version: "1.6.0",
-    extra: {
-      fact: 'kittens are cool',
-      user_pool_id: process.env.EXPO_PUBLIC_COGNITO_USER_POOL_ID,
-      user_pool_client_id: process.env.EXPO_PUBLIC_COGNITO_CLIENT_ID,
-      identity_pool_id: process.env.EXPO_PUBLIC_COGNITO_IDENTITY_POOL_ID,
-      aws_region: process.env.EXPO_PUBLIC_COGNITO_REGION,
+  "expo": {
+    "name": "Story Tailor",
+    "slug": "Story-Tailor",
+    "version": "1.6.0",
+    "orientation": "portrait",
+    "icon": "./assets/images/icon.png",
+    "scheme": "storytailor",
+    "jsEngine": "hermes",
+    "userInterfaceStyle": "automatic",
+    "owner": "bybraincloud",
+    "splash": {
+      "image": "./assets/images/splash.png",
+      "resizeMode": "contain",
+      "backgroundColor": "#ffffff"
     },
-    ios: {
-      bundleIdentifier: "com.bybraincloud.Story-Tailor",
+    "ios": {
+      "supportsTablet": true,
+      "bundleIdentifier": "com.bybraincloud.Story-Tailor",
+      "buildNumber": "6"
     },
-    android: {
-      package: "com.bybraincloud.storytailor",
+    "android": {
+      "adaptiveIcon": {
+        "foregroundImage": "./assets/images/adaptive-icon.png",
+        "backgroundColor": "#ffffff"
+      },
+      "package": "com.bybraincloud.StoryTailor"
     },
-    platforms: ["ios", "android", "web"],
-  },
+    "web": {
+      "bundler": "metro",
+      "output": "static",
+      "favicon": "./assets/images/favicon.png"
+    },
+    "plugins": [
+      "expo-router",
+      "expo-secure-store"
+    ],
+    "experiments": {
+      "typedRoutes": true
+    },
+    "extra": {
+      "router": {
+        "origin": false,  
+      },
+      "fact": 'kittens are cool',
+      "user_pool_id": process.env.EXPO_PUBLIC_COGNITO_USER_POOL_ID,
+      "user_pool_client_id": process.env.EXPO_PUBLIC_COGNITO_CLIENT_ID,
+      "identity_pool_id": process.env.EXPO_PUBLIC_COGNITO_IDENTITY_POOL_ID,
+      "aws_region": process.env.EXPO_PUBLIC_COGNITO_REGION,
+      "eas": {
+        "projectId": "4fdc003f-4775-470e-9499-f2e0f3494fba"
+      }
+    }
+  }
 };
+
