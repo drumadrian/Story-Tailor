001f81dfeba7f9a0f80865642b251f6c96cb95c1
Adrian Drummond <adrian.drummond@gmail.com>
2024-10-31 20:45:41 -0700

    format update

--- Story-Tailor/app.config.js
+++ Story-Tailor/app.config.js
@@ -2,6 +2,9 @@ import 'expo-env';
 
 export default {
   expo: {
+    name: "Story Tailor",
+    slug: "Story-Tailor",
+    version: "1.6.0",
     extra: {
       fact: 'kittens are cool',
       user_pool_id: process.env.EXPO_PUBLIC_COGNITO_USER_POOL_ID,
@@ -9,5 +12,12 @@ export default {
       identity_pool_id: process.env.EXPO_PUBLIC_COGNITO_IDENTITY_POOL_ID,
       aws_region: process.env.EXPO_PUBLIC_COGNITO_REGION,
     },
+    ios: {
+      bundleIdentifier: "com.bybraincloud.Story-Tailor",
+    },
+    android: {
+      package: "com.bybraincloud.storytailor",
+    },
+    platforms: ["ios", "android", "web"],
   },
 };
